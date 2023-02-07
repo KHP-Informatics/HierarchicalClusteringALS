1e4a51e36b6a55636d2cd4965441ee2b3404020d
Heather Marriott <71556548+hevmarriott@users.noreply.github.com>
2023-02-07 14:51:47 +0000

    Create transcriptional_age.R

---
+++ data_processing/transcriptional_age.R
@@ -0,0 +1,16 @@
+# install packages
+BiocManager::install("RNAAgeCalc")
+library(RNAAgeCalc)
+
+# define arguments 
+args[1] = expression_data
+args[2] = tissue_type
+args[3] = population
+args[4] = out_dir
+
+# load in expression data
+exprdata <- read.table(expression_data)
+
+# run the program and save the text file
+res = predict_age(exprdata = exprdata, tissue = tissue_type, exprtype = "counts", idtype = "ENSEMBL", stype = population, chronage = chronage)
+write.table(res, sprintf("%sRNAAge.txt", out_dir))
